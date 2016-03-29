26f090dcf5bc97bcb6f132b867c2674b7530d74b
JKetelaar <jeroen@ketelaar.me>
2016-03-29 19:48:11 +0200

    [BUGFIX] Set password for user creation

--- .travis/.initialize.db.sh
+++ .travis/.initialize.db.sh
@@ -1,3 +1,3 @@
 mysql -u root -e "CREATE DATABASE hhspring DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
-mysql -u root -e "CREATE USER 'hhuser'; grant all on hhspring.* to hhuser;"
-mysql -u root -e "CREATE USER 'hhuser'@'localhost'; grant all on hhspring.* to hhuser@localhost;"
+mysql -u root -e "CREATE USER 'hhuser' IDENTIFIED BY 'boem'; grant all on hhspring.* to hhuser;"
+mysql -u root -e "CREATE USER 'hhuser'@'localhost' IDENTIFIED BY 'boem'; grant all on hhspring.* to hhuser@localhost;"
